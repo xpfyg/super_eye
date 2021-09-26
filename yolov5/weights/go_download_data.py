4cbe0feedd588569de73fee5f1f7715f26702001
chihuan <chihuan1>
2021-09-26 14:15:25 +0800

    add

---
+++ yolov5/weights/go_download_data.py
@@ -0,0 +1,8 @@
+
+from utils.downloads import attempt_download, gdrive_download
+
+# attempt_download('yolov5s.pt')
+# attempt_download('yolov5m.pt')
+# attempt_download('yolov5l.pt')
+# attempt_download('yolov5x.pt')
+gdrive_download('1n_oKgR81BJtqk75b00eAjdv03qVCQn2f','coco128.zip')
